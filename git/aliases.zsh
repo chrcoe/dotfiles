fbafe35fbce4114aa10bceaa968d675ad4c9a5ac
Chris Coe <chrcoe@ieee.org>
2015-04-28 14:38:28 -0400

    updated git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,6 +7,7 @@ then
 fi
 
 # The rest of my fun git aliases
+alias gfl='git fetch && git pull --prune'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -17,3 +18,4 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias ga='git add'
