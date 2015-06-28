22459d2adbb2f6c858642f20d7f81bb794173b06
Chris Coe <chrcoe@ieee.org>
2015-06-28 09:53:24 -0400

    updated screenlayout scripts

--- screenlayout.symlink/docked-single_DP2-2.sh
+++ screenlayout.symlink/docked-single_DP2-2.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+xrdb -merge $HOME/.Xresources
 xrandr --dpi 96
 xrandr --output eDP1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
