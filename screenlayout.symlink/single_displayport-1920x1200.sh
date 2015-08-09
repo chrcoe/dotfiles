85bd665f790e74030b805da1a6d798fe573a5df5
Chris Coe <chrcoe@ieee.org>
2015-08-09 09:50:15 -0400

    zsh exports structure update and new display script

--- screenlayout.symlink/single_displayport-1920x1200.sh
+++ screenlayout.symlink/single_displayport-1920x1200.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+vim $HOME/.Xresources
 xrdb -merge $HOME/.Xresources
 xrandr --dpi 96
 xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --mode 1920x1200 --pos 0x0 --rotate normal --output eDP1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP2 --off
--- zsh/envexports.zsh
+++ zsh/envexports.zsh
@@ -1,2 +1,3 @@
+export CHROME_BIN=/usr/bin/chromium-dev
 export WORKON_HOME=~/.venv
 source /usr/bin/virtualenvwrapper.sh
