fd6520ed3a3433919d71f69753aa864e3f56f75c
Chris Coe <chrcoe@ieee.org>
2015-08-11 08:31:23 -0400

    added i3status.conf and updated git aliases

--- git/completion.zsh
+++ git/completion.zsh
@@ -2,8 +2,10 @@
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
 completion='$(brew --prefix)/share/zsh/site-functions/_git'
+# completion="$HOME/.dotfiles/git/_git"
 
 if test -f $completion
 then
+  echo $completion
   source $completion
 fi
--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -46,7 +46,9 @@ ZSH_THEME="bira"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+# plugins=(git)
+# plugins=(git-extras gitfast autopep8 docker)
+plugins=(git-flow gitfast autopep8 docker sudo)
 
 # User configuration
 
