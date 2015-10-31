42acbed3252d509fc0ba0317dd0bcb9f9e9e9585
Chris Coe <chrcoe@ieee.org>
2015-10-31 15:55:57 -0400

    updated shortcut keys in i3 for dock/undock

--- screenlayout.symlink/undocked.sh
+++ screenlayout.symlink/undocked.sh
@@ -5,4 +5,4 @@ xrdb -merge $HOME/.Xresources
 xrandr --dpi 168
 xrandr --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 # restart i3
-restart
+i3-msg restart
