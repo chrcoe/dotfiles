df33a0e85ef5109f85da6457f77e279179c503f2
Chris Coe <chrcoe@ieee.org>
2015-10-31 15:30:43 -0400

    added hlsearch to vimrc and edited dock/undock scripts

--- screenlayout.symlink/undocked.sh
+++ screenlayout.symlink/undocked.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-vim $HOME/.Xresources
+sed -i 's/#define DPI 96/#define DPI 168/g' $HOME/.Xresources
 xrdb -merge $HOME/.Xresources
 xrandr --dpi 168
 xrandr --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
