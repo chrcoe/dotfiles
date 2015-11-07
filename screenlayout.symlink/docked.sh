791e1e1f49880f4c144dc05e4e7dfaac6eb260d8
Chris Coe <chrcoe@ieee.org>
2015-11-07 07:27:40 -0500

    updated docked scrip

--- screenlayout.symlink/docked.sh
+++ screenlayout.symlink/docked.sh
@@ -3,6 +3,8 @@
 sed -i 's/#define DPI 168/#define DPI 96/g' $HOME/.Xresources
 xrdb -merge $HOME/.Xresources
 xrandr --dpi 96
-xrandr --output eDP1 --mode 2560x1440 --pos 3840x0 --rotate normal --output DP1 --off --output DP2-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output eDP1 --mode 2560x1440 --pos 3840x0 --rotate normal --output DP1 --off --output DP2-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2-2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+# xrandr --output eDP1 --mode 2560x1440 --pos 3840x0 --rotate normal --output DP1 --off --output DP2-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 # restart i3
 i3-msg restart
+#!/bin/sh
