a99939cc6c09f16f4b57e3f19f086310c8af6c82
Chris Coe <chrcoe@ieee.org>
2015-12-05 07:45:28 -0500

    updated browser var

--- system/env.zsh
+++ system/env.zsh
@@ -1,4 +1,5 @@
 # export EDITOR='atom'
 
 export EDITOR='vim'
-export BROWSER='chromium-dev --force-device-scale-factor=1.75'
+# export BROWSER='chromium-dev --force-device-scale-factor=1.75'
+export BROWSER='chromium'
--- zsh/envexports.zsh
+++ zsh/envexports.zsh
@@ -1,3 +1,4 @@
-export CHROME_BIN=/usr/bin/chromium-dev
+# export CHROME_BIN=/usr/bin/chromium-dev
+export CHROME_BIN=/usr/bin/chromium
 export WORKON_HOME=~/.venv
 source /usr/bin/virtualenvwrapper.sh
