b7e76eb7bd5375faa6f986fb5b5c2fe8c661e55b
Chris Coe <chris.coe@decisiondesk.com>
2016-11-30 07:29:52 -0500

    updated git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,6 +17,7 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
+# alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias ga='git add'
 alias grm='git rm --cached'
