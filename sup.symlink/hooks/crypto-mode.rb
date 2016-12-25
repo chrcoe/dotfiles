76005c5a6b7393caaa1454be25baffd3fa8c6c04
chrcoe <chrcoe@ieee.org>
2016-12-24 22:02:18 -0500

    lots of fixes for gpg with sup

---
+++ sup.symlink/hooks/crypto-mode.rb
@@ -0,0 +1,13 @@
+# crypto-mode
+# -----------
+# File: /home/chrcoe/.sup/hooks/crypto-mode.rb
+# Modifies cryptography settings based on header and message content, before
+# editing a new message. This can be used to set, for example, default cryptography
+# settings.
+    # Variables:
+        # header: a hash of headers. See 'signature' hook for documentation.
+        # body: an array of lines of body text.
+        # crypto_selector: the UI element that controls the current cryptography setting.
+    # Return value:
+        # none
+
