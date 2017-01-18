ab0d58e3f1a15adde184239ac880b82e7cee832e
chrcoe <chrcoe@ieee.org>
2017-01-18 06:30:30 -0500

    rbenv setup

--- zsh/envexports.zsh
+++ zsh/envexports.zsh
@@ -5,10 +5,13 @@ source $(which virtualenvwrapper.sh)
 source $HOME/.bash_profile
 export VISUAL="vim"
 
+export PATH="$HOME/.rbenv/shims:$PATH"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+
+export MSF_DATABASE_CONFIG=/opt/metasploit/config/database.yml
+
 if [[ $(hostname) == 'vagrant' ]]; then
     export DEFAULT_USER='vagrant'
 else
     export DEFAULT_USER='chrcoe'
 fi
-
-eval "$(rbenv init -)"
