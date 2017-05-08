1177cd0ab62e63dea05e91df2216022e64627f33
chrcoe <chrcoe@ieee.org>
2017-05-08 10:59:17 -0400

    updated soundtoggle scripts

--- i3/i3.symlink/soundtoggle.sh
+++ i3/i3.symlink/soundtoggle.sh
@@ -6,5 +6,9 @@
 amixer -c 0 -q set IEC958 toggle
 # card 1: builtin/PCM
 amixer -c 1 -q set Master toggle
+amixer -c 1 -q set Speaker 100% unmute
+amixer -c 1 -q set Headphone 100% unmute
 # card 2: Docking Station (shown as USB audio)
 amixer -c 2 -q set Master toggle
+amixer -c 2 -q set Speaker 100% ummute
+amixer -c 2 -q set Headphone 100% unmute
