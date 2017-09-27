516e50a9a4eeab0c625be1e814a9d9b205906bee
Chris Coe <ccoe@onshift.com>
2017-09-27 13:38:56 -0400

    updated more dotfile shenanigans

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,1 +1,4 @@
 # none yet - go ahead and add some as needed !
+
+alias docker_clean="docker rmi $(docker images | grep "^<none>" | awk "{print $3}")"
+alias docker_clean_containers="docker rm $(docker ps -a -q)"
