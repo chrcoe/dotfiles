4550ad55e618554e7d0c8d79e1bd511e4b60e1f5
Chris Coe <chrcoe01@gmail.com>
2017-12-05 07:48:31 -0500

    added greptodo alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,4 @@ alias vim=nvim
 alias vimdiff='nvim -d'
 alias devbox!='. ~/.dotfiles/tmux/devbox'
 alias tree='tree -Capug --dirsfirst'
+alias greptodo="grep -RE 'TODO:|FIXME:|NOTE:|OPTIMIZE:|XXX:|REFACTOR:'"
