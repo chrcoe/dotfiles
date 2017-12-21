01d7ae1509858a151483397492b807b3b324b214
Chris Coe <chrcoe01@gmail.com>
2017-12-21 14:14:01 -0500

    added time fix alias for ubuntu

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias vimdiff='nvim -d'
 alias devbox!='. ~/.dotfiles/tmux/devbox'
 alias tree='tree -Capug --dirsfirst'
 alias greptodo="grep -RE 'TODO:|FIXME:|NOTE:|OPTIMIZE:|XXX:|REFACTOR:'"
+alias fixthetime="sudo systemctl stop ntp.service && sudo ntpd -gq && sudo systemctl start ntp.service"
