4dad22d449bf941cbbbec6dddb505607acb91cde
chrcoe <chrcoe01@gmail.com>
2018-08-28 09:23:32 -0400

    added kernel cleaning alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,4 @@ alias greptodo="grep -RE 'TODO:|FIXME:|NOTE:|OPTIMIZE:|XXX:|REFACTOR:'"
 alias fixthetime="sudo systemctl stop ntpd.service && sudo ntpd -gq && sudo systemctl start ntpd.service"
 
 alias ll="ls -alhi"
+alias cleankernels="sudo package-cleanup --oldkernels --count=1"
