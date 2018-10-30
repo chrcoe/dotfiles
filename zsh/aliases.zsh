9fad2fbfc753227a9a3dc75c138660edac37bdf7
chrcoe <chrcoe01@gmail.com>
2018-10-30 06:41:34 -0400

    updated alias for fixthetime

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias devbox!='. ~/.dotfiles/tmux/devbox'
 alias tree='tree -Capug --dirsfirst'
 alias greptodo="grep -RE 'TODO:|FIXME:|NOTE:|OPTIMIZE:|XXX:|REFACTOR:'"
 #alias fixthetime="sudo systemctl stop ntp.service && sudo ntpd -gq && sudo systemctl start ntp.service"
-alias fixthetime="sudo systemctl stop ntpd.service && sudo ntpd -gq && sudo systemctl start ntpd.service"
+alias fixthetime="sudo systemctl restart network.service && sudo systemctl stop ntpd.service && sudo ntpd -gq && sudo systemctl start ntpd.service"
 
 alias ll="ls -alhi"
 alias cleankernels="sudo package-cleanup --oldkernels --count=1"
