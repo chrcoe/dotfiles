d34352677b40a8b1f94f30b2e01291b620f8566d
chrcoe <chrcoe01@gmail.com>
2018-12-04 07:29:18 -0500

    homebrew hardening stuff

--- zsh/envexports.zsh
+++ zsh/envexports.zsh
@@ -7,7 +7,11 @@ export PATH=$PATH:/usr/local/go/bin
 source $(which virtualenvwrapper.sh)
 export VISUAL="vim"
 
+# homebrew hardening
 export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_GITHUB_API=1
+export HOMEBREW_NO_INSECURE_REDIRECT=1
 
 export POWERLINE_CONFIG_COMMAND=$(which powerline-config)
 
