546736cf64cc2ec4e475bc0c6af2aa078a6fabd2
chrcoe <chrcoe01@gmail.com>
2019-03-13 08:29:26 -0400

    sshvm alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,3 +15,4 @@ alias fuc="cat $HOME/.fuc | grep -i"
 alias startvm='VBoxManage startvm "devbox_centos7" --type headless'
 alias stopvm='VBoxManage controlvm "devbox_centos7" poweroff --type headless'
 alias listvm='VBoxManage list runningvms'
+alias sshvm='ssh devbox-centos'
