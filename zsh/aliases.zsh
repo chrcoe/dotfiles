cb967caf533f8be831211c81dfd16b7363055e81
Chris Coe <chris.coe@boldpenguin.com>
2019-05-13 10:35:31 -0400

    update for adding RVM

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,6 +7,7 @@ alias tree='tree -Capug --dirsfirst'
 alias greptodo="grep -RE 'TODO:|FIXME:|NOTE:|OPTIMIZE:|XXX:|REFACTOR:'"
 #alias fixthetime="sudo systemctl stop ntp.service && sudo ntpd -gq && sudo systemctl start ntp.service"
 alias fixthetime="sudo systemctl restart network.service && sudo systemctl stop ntpd.service && sudo ntpd -gq && sudo systemctl start ntpd.service"
+alias fixthetime-ubuntu="sudo netplan apply && sudo systemctl restart systemd-timesyncd"
 
 alias ll="ls -alhi"
 alias cleankernels="sudo package-cleanup --oldkernels --count=1"
