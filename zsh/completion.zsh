49b73eb0af266970560b9fb897f9ce81a846ff25
chrcoe <chris.coe@boldpenguin.com>
2021-03-04 15:57:29 -0500

    updates

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,6 +3,3 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
-
-# requires awscli --> $ pip install awscli --upgrade --user
-source $(which aws_zsh_completer.sh)
